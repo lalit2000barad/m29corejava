67b3fa2ab04825132e0826341c87357259c5ed9e
DELL <DELL@DESKTOP-R3EJLL1>
2023-04-24 17:09:02 +0530

    second commit

---
+++ daysix/src/org/tnsiff/methodoverloading/MethodOverloadingExample.java
@@ -0,0 +1,28 @@
+// progarm to demo on method overloading
+package org.tnsiff.methodoverloading;
+
+public class MethodOverloadingExample {
+
+	
+	//method overloading by passing different types ofargument
+	public int multiplication(int a, int b)
+	{
+		return a*b;
+	}
+	public float multiplication(float a, float b)
+	{
+		return a*b;
+	}
+	
+	public float multiplication(int a, float b)
+	{
+		return a*b;
+	}
+	
+	public float multiplication(float a, int b)
+	{
+		return a*b;
+	}
+	
+	
+}
