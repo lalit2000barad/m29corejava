07c13e4c4fde2115342b07f7d103adf6ff9ca179
lalit2000barad <lalitb@sjcem.edu.in>
2023-06-15 22:29:46 +0530

    first commit

---
+++ daynineteen/src/org/tnsif/streamapi/MappingExample.java
@@ -0,0 +1,15 @@
+//program to demonstrate on the map operation
+package org.tnsif.streamapi;
+import java.util.Arrays;
+import java.util.List;
+public class MappingExample {
+
+	public static void main(String[] args) {
+		//using collection
+		List<String>obj=Arrays.asList("Ankita","Parag","Prathmesh");
+		obj.stream().map(i->i.length()).forEach((i)->System.out.print(i+" "));
+		
+
+	}
+
+}
