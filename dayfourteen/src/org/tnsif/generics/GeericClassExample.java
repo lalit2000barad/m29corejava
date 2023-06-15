b517e4e5c92eec4092a07dcf1aeda3b6ac54105f
lalit2000barad <lalitb@sjcem.edu.in>
2023-06-15 22:27:03 +0530

    first commit

---
+++ dayfourteen/src/org/tnsif/generics/GeericClassExample.java
@@ -0,0 +1,7 @@
+package org.tnsif.generics;
+
+public class GeericClassExample {
+
+	
+	
+}
---
+++ dayfourteen/src/org/tnsif/generics/GenericMethodExample.java
@@ -0,0 +1,10 @@
+package org.tnsif.generics;
+
+public class GenericMethodExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
